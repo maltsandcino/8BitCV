b8c865085df4e060c399d78da18ff11fcbe3d594
Tom <thomasjh@gmail.com>
2025-06-22 18:53:23 +0200

    Changing message for smaller screens, adding links.

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     pc2: 'Check out my github repository. My user name is <a href="https://github.com/maltsandcino">@maltsandcino</a>',
-    welcome: "Welcome to Tom Harrison's 8Bit-inspired Portfolio. To move around, use the cursor keys (the direction-arrows on your keyboard) or click / tap and hold on where you want to go. Check out the computer, my desk, the bookshelf, or many of the other items you see to get a better idea about some of the projects I've worked on. Simply walk up to them to trigger the event. If you'd rather go directly to my Github Profile, my PortfoliOS environment, view my Resume, or go to my LinkedIn click on these links now."
+    welcome: "Welcome to Tom Harrison's 8Bit-inspired Portfolio. Learn more about me and my projects by going to the objects in the room. Or, directly visit my <a href='https://github.com/maltsandcino'>github</a>, <a href='https://www.linkedin.com/in/tharrisoncs/'>LinkedIn</a>, <a href='https://portfolios-smoky.vercel.app/'>PortfoliOS</a>, or view my <a href='https://github.com/maltsandcino/8BitCV/blob/main/public/Thomas%20Harrison%20Resume.pdf'>Resume</a> directly on github." 
 }
