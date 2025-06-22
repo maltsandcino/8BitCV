01cfb7ff5ce24e94d6377d5bd45d5683de6edaf8
Tom <thomasjh@gmail.com>
2025-06-22 18:54:39 +0200

    phrasing

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     pc2: 'Check out my github repository. My user name is <a href="https://github.com/maltsandcino">@maltsandcino</a>',
-    welcome: "Welcome to Tom Harrison's 8Bit-inspired Portfolio. Learn more about me and my projects by going to the objects in the room. Or, directly visit my <a href='https://github.com/maltsandcino'>github</a>, <a href='https://www.linkedin.com/in/tharrisoncs/'>LinkedIn</a>, <a href='https://portfolios-smoky.vercel.app/'>PortfoliOS</a>, or view my <a href='https://github.com/maltsandcino/8BitCV/blob/main/public/Thomas%20Harrison%20Resume.pdf'>Resume</a> directly on github." 
+    welcome: "Welcome to Tom Harrison's 8Bit-inspired Portfolio. Learn more about me and my projects by going to the objects in the room. Or, directly visit my <a href='https://github.com/maltsandcino'>github</a>, <a href='https://www.linkedin.com/in/tharrisoncs/'>LinkedIn</a>, <a href='https://portfolios-smoky.vercel.app/'>PortfoliOS</a>, or view my <a href='https://github.com/maltsandcino/8BitCV/blob/main/public/Thomas%20Harrison%20Resume.pdf'>resume</a> on Github." 
 }
